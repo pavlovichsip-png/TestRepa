6ffdb950fcbd256f95102914d4115191bc6b10c8
solovey <pavlovich.sip@gmail.com>
2026-09-21 22:18:07 +0300

    2 Класс Computer

---
+++ src/Computer.java
@@ -0,0 +1,9 @@
+public class Computer {
+    String name;
+    int memory_size;
+
+    public Computer(String name, int memory_size) {
+        this.name = name;
+        this.memory_size = memory_size;
+    }
+}
