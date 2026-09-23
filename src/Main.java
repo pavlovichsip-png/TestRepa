bebf85aeb30674c23dc41119be78b6268c27dda6
solovey <pavlovich.sip@gmail.com>
2026-09-24 02:15:39 +0300

    для конфлика в фиче4 изменяю мейн

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Rect rect = new Rect();
-        rect.drawRect();
+        ICircle circle = new Circle();
+        circle.drawCircle();
     }
 }
